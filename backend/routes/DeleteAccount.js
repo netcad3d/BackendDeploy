6c19acd4ba656b109969b9b238b9b1817624488e
Eren <erentarak1907@gmail.com>
2022-08-30 18:40:30 +0300

    back

--- backend/routes/DeleteAccount.js
+++ backend/routes/DeleteAccount.js
@@ -2,7 +2,7 @@ const router=require('express').Router();
 const requireAuth=require('../middlewares/requireAuth');
 const mongoose = require("mongoose");
 const { User } = require("../models/User");
-const File  = require("../models/File");
+const {File}  = require("../models/File");
 const upload = require("../middlewares/upload");
 const Grid = require("gridfs-stream");
 
@@ -31,18 +31,10 @@ router.delete('/:id', async (req, res) => {
 	const user = await User.findOne({ _id: id });
 	if (!user) return res.status(400).send({ message: "User not found" });
 
-	const files = await File.find({ userId: id });
 
-	if (!files) return res.status(400).send("No Files Found");
+	
   
-	await File.deleteMany({ userId:id });
-  
-	files.forEach((file) => {
-	  //! Delete file from GridFS
-	  gfs.delete(file.fileId, (err) => {
-		if (err) return res.status(400).send(err);
-	  });
-	});
+	
 	
 /*	userId=user._id;
 	const files = await File.find({userId: userId });

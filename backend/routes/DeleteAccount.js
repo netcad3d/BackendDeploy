51d27959a3c342f3297ee775eba27500e93054ac
Eren <erentarak1907@gmail.com>
2022-08-30 18:12:10 +0300

    test account

--- backend/routes/DeleteAccount.js
+++ backend/routes/DeleteAccount.js
@@ -3,6 +3,22 @@ const requireAuth=require('../middlewares/requireAuth');
 const mongoose = require("mongoose");
 const { User } = require("../models/User");
 const { File } = require("../models/File");
+const upload = require("../middlewares/upload");
+const Grid = require("gridfs-stream");
+
+//! DB Connection
+const mongoUri = process.env.MONGO_URI;
+const connection = mongoose.createConnection(mongoUri);
+
+//! GridFS Storage
+let gfs;
+
+connection.once("open", () => {
+  //! Init stream
+  gfs = new mongoose.mongo.GridFSBucket(connection.db, {
+    bucketName: "uploads",
+  });
+});
 
 
 // cascade delete user and its files
@@ -14,6 +30,19 @@ router.delete('/:id', async (req, res) => {
 
 	const user = await User.findOne({ _id: id });
 	if (!user) return res.status(400).send({ message: "User not found" });
+
+	const files = await File.find({ userId: id });
+
+	if (!files) return res.status(400).send("No Files Found");
+  
+	await File.deleteMany({ userId:id });
+  
+	files.forEach((file) => {
+	  //! Delete file from GridFS
+	  gfs.delete(file.fileId, (err) => {
+		if (err) return res.status(400).send(err);
+	  });
+	});
 	
 /*	userId=user._id;
 	const files = await File.find({userId: userId });
